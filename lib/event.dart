dcf8d28e5169d10fe25231893b6ff9582388dbc0
JedLeas <jedleasyt@gmail.com>
2025-02-20 20:02:02 +0000

    Restarting Event.dart

--- lib/event.dart
+++ lib/event.dart
@@ -1,98 +1,79 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'connection.dart';
 import 'utils.dart';
 
-bool hasAgreed = false;
-
+var hasAgreed = false;
 
 class EventScreen extends StatefulWidget {
-  const EventScreen({super.key});
+const EventScreen({super.key});
 
-  @override
-  State<EventScreen> createState() => _EventScreenState();
+@override
+State<EventScreen> createState() => _EventScreen();
 }
 
-class _EventScreenState extends State<EventScreen> {
+class _EventScreen extends State<EventScreen> {
 
-  bool hasLoaded = false;
+bool hasLoaded = false;
+Container? eventsContainer;
+List<Widget> friendEntries = List<Widget>.empty(growable: true);
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
 
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Event",
-          style: TextStyle(
-            color: DEFAULT_WHITE,
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              await Navigator.push(context, MaterialPageRoute(builder: (context) => EventSettingsScreen()));
-              setState(() {});
-            },
-            icon: Icon(Icons.settings_outlined),
-          )
-        ],
+  appBar: AppBar(
+    title: Text("Events"),
+    centerTitle: true,
+    actions: [
+      IconButton(
+        padding: EdgeInsets.only(right: 20),
+        icon: Icon(Icons.person_add), // Icon for the button
+        onPressed: () {
+          // Action when the button is pressed
+          //Navigator.push(context,  MaterialPageRoute(builder: (context) => AddFriendScreen()));
+        },
       ),
-      
-      body: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 20,
-        ),
-        child: Column(
-          spacing: 20,
-          children: [
-
-            Text("**NOTE PLEASE READ** By enabling the event feature you acknowledge that the Blood Alcohol Measurement (graph) is simply an estimate for fun "
-                "and should not be used as a real measurement of the users sobriety."),
+    ],
+  ),
 
 
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white10,
-                border: Border.all(color: Colors.black)
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Enable Event Screen",
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  CupertinoSwitch(
-                    value: hasAgreed,
-                    onChanged: (value) {
-                      setState(() {
-                        hasAgreed = !hasAgreed;
-                      });
-                    },
-                  )
-                ],
-              ),
-            ),
-
-            Text("Events can be turned off any at time in the Event Settings (top right)",
-              textAlign: TextAlign.center,
-            )
-          ],
+  body: RefreshIndicator(
+    onRefresh: checkProfile,
+      child: Container(
+        alignment: Alignment.center,
+        child: hasLoaded ? eventsContainer : CircularProgressIndicator(
+          color: DEFAULT_WHITE,
         ),
       ),
 
+  ),
+
+);
+}
+
+@override
+void initState() {
+  super.initState();
+  checkProfile();
+}
+
+Future<void> checkProfile() async {
+  var profile = await getBACProfile();
+  debugPrint("Profile Data: $profile");
+
+  setState(() {
+    hasLoaded = true;
+  });
+  if (profile[0] != 1) {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => EventSettingsScreen()),
     );
+    setState(() {}); // Update the UI if needed
   }
+}
 }
 
 class EventSettingsScreen extends StatefulWidget {
@@ -109,8 +90,8 @@ class _EventSettingsScreenState extends State<EventSettingsScreen> {
       appBar: getDefaultAppBar(context, "Event Screen"),
       body: Container(
         padding: const EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 20
+            vertical: 10,
+            horizontal: 20
         ),
         child: Column(
 
